c6b64f33f20db0e80824c2173e1b129be21553c1
ErJump <fedelijunior@gmail.com>
2022-07-08 13:50:59 +0200

    Removes key toggle from messages

--- js/main.js
+++ js/main.js
@@ -11,19 +11,16 @@ const app = new Vue({
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent',
-                        toggle: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
                         status: 'sent',
-                        toggle: false
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
                         status: 'received',
-                        toggle: false,
                     }
                 ],
             },
